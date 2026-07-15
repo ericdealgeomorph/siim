0aa40a7877b48de253e06bdef3828000671a34ae
Eric Deal <eric.deal@icloud.com>
2026-07-15 11:49:47 -0600

    Sync from siim-dev @ 19ccb5b

--- siim/__init__.py
+++ siim/__init__.py
@@ -34,7 +34,7 @@ import os as _os
 # within process noise on siim's non-nested prange kernels.
 _os.environ.setdefault("NUMBA_THREADING_LAYER", "workqueue")
 
-__version__ = "0.5.0"
+__version__ = "0.9.0"
 
 
 _LAZY = ("analytical_steady_state_solution", "GeneralProfile",
